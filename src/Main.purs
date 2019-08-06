dfe605f42539f317d4218d507927528630c9f2fb
Gerard Paapu <gerard.paapu@xero.com>
2019-08-06 16:51:34 +1200

    Features: No tests...
    So now we use the "blocks" API to get a nicer looking message.
    
    We thread if:
    - The message that triggers us is in a thread
    - The message that triggers us has multiple search terms
    
    Ok, I've got to figure out how to test this sort of thing... I can't
    keep releasing it to lambda to "see what happens".

--- src/Main.purs
+++ src/Main.purs
@@ -3,15 +3,14 @@ module Main where
 import Prelude
 
 import Control.Monad.Error.Class (class MonadThrow)
-import Control.Parallel (parTraverse)
+import Control.Parallel (class Parallel, parTraverse)
 import Control.Promise (Promise, fromAff)
 import Data.Array ((!!))
-import Data.Compactable (compact)
-import Data.Foldable (class Foldable, foldl, intercalate)
+import Data.Array as Array
+import Data.Foldable (class Foldable, foldl)
 import Data.Maybe (Maybe(..))
-import Data.Traversable (for)
-import Effect (Effect)
-import Effect.Aff (Aff, error, launchAff_, throwError)
+import Data.Traversable (class Traversable, for_)
+import Effect.Aff (Aff, error, throwError)
 import Effect.Class (liftEffect)
 import Effect.Console as Console
 import Effect.Exception (Error)
@@ -23,6 +22,7 @@ import Slack.API (EventBody(..))
 import Slack.API as Slack
 
 
+inParallel :: forall f eff par a b. Parallel par eff => Traversable f => f a -> (a -> eff b) -> eff (f b)
 inParallel a b = parTraverse b a
 
 type Response
@@ -32,9 +32,7 @@ type Response
     }
 
 type LambdaEvent
-  = { body :: String
-    }
-
+  = { body :: String }
 
 handler :: E.EffectFn1 LambdaEvent (Promise Response)
 handler = E.mkEffectFn1 $ \ev -> fromAff $ handle ev
@@ -46,7 +44,7 @@ handler = E.mkEffectFn1 $ \ev -> fromAff $ handle ev
       botToken <- Slack.BotToken <$> getEnv "BOT_TOKEN"
 
       params :: Slack.Event <- J.readJSON ev.body # orThrow "Invalid request body"
-      
+
       if params.token /= verificationToken then
         throwError (error "Mismatched Verification Token")
       else if params.type == "url_verification" then do
@@ -57,19 +55,31 @@ handler = E.mkEffectFn1 $ \ev -> fromAff $ handle ev
                }
       else do
         case params.event of
-          Just (Message { channel, text }) -> do
+          Just (Message { channel, text, ts, thread_ts }) -> do
             token <- HS.authenticate clientID clientSecret
-            images <- inParallel (parseMessage text) $ \match -> do
-                results <- HS.cards token.access_token match
-                pure $ _.image <$> results.cards !! 0
+            searchResults <- inParallel (parseMessage text) $ \term -> do
+                results <- HS.cards token.access_token term
+                pure $ { result: results.cards !! 0
+                       , term
+                       }
 
-            let lines = intercalate "\n" $ compact images
-            if lines /= "" then
-              Slack.postMessage botToken channel lines
-            else
-              pure unit
+            let thread =
+                  if Array.length searchResults > 1 then
+                    Just ts
+                  else
+                    thread_ts
+
+            for_ searchResults $ \item ->
+              case item.result of
+                Just card -> do
+                  let block = Slack.imageBlock card.name card.image
+                  Slack.postMessage botToken channel thread card.name (Just [block])
+
+                Nothing -> do
+                  let msg = "No results for: " <> item.term
+                  Slack.postMessage botToken channel thread msg  Nothing
           _ -> pure unit
-              
+
         pure $ { statusCode: 200.0
                , body: ""
                , headers: { "Content-Type": "application/json" }
@@ -77,9 +87,9 @@ handler = E.mkEffectFn1 $ \ev -> fromAff $ handle ev
 
 foreign import parseMessage :: String -> Array String
 
-    
+
 getEnv :: String -> Aff String
-getEnv a = 
+getEnv a =
   Process.lookupEnv a
     # liftEffect
     >>= orThrow ("missing env variable " <> a)
--- src/Slack/API.purs
+++ src/Slack/API.purs
@@ -24,22 +24,52 @@ derive newtype instance verificationTokenEq :: Eq VerificationToken
 derive newtype instance readForeignToken :: J.ReadForeign VerificationToken
 derive newtype instance showToken :: Show VerificationToken
 
-postMessage :: BotToken -> String -> String -> Aff Unit
-postMessage (BotToken token) channel text = do
+newtype Thread = Thread String
+derive newtype instance readForeignThread :: J.ReadForeign Thread
+derive newtype instance writeForeignThread :: J.WriteForeign Thread
+
+newtype Channel = Channel String
+derive newtype instance readForeignChannel :: J.ReadForeign Channel
+derive newtype instance writeForeignChannel :: J.WriteForeign Channel
+
+
+postMessage :: BotToken -> Channel -> Maybe Thread -> String -> Blocks -> Aff Unit
+postMessage (BotToken token) (Channel channel) thread text blocks = do
   resp <- post body $ mempty
     <> H.protocol := "https:"
     <> H.hostname := "slack.com"
     <> H.path := "/api/chat.postMessage"
     <> H.headers := H.RequestHeaders (fromHomogeneous h)
-  rt <- readToEnd UTF8 (H.responseAsStream resp)
-  liftEffect $ Console.log rt
   pure unit
     where
-    body = J.writeJSON { channel, text }
+    body = J.writeJSON { channel, text, blocks, thread_ts: thread }
     h = { "Content-Type": "application/json; charset=utf-8"
         , "Content-Length": Buffer.byteLength body UTF8 # show
         , "Authorization": "Bearer " <> token
         }
+
+type Block
+  = { type :: String
+    , title :: { type :: String
+               , text :: String
+               , emoji :: Boolean
+               }
+    , image_url :: String
+    , alt_text :: String
+    }
+
+type Blocks = Maybe (Array Block)
+
+imageBlock :: String -> String -> Block
+imageBlock title url =
+  { type: "image"
+  , title: { type: "plain_text"
+           , text: title
+           , emoji: false
+           }
+  , image_url: url
+  , alt_text: title
+  }
 
 type EventWrapper r
    = { token :: VerificationToken
@@ -49,14 +79,16 @@ type EventWrapper r
      }
 
 data EventBody
-   = Message { channel :: String
+   = Message { channel :: Channel
              , text :: String
+             , ts :: Thread
+             , thread_ts :: Maybe Thread
              }
    | Unknown
 
 
 type Event = EventWrapper EventBody
-     
+
 instance readForeignEventBody ::
   ReadForeign EventBody where
   readImpl f = do
